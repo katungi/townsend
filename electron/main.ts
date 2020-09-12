34c2bbce0f570dc242fb50899eb0ffffdd31f727
katungi <dankatdennis@gmail.com>
2020-09-12 20:13:32 +0300

    feat: Added Menu

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow , Menu} from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
@@ -24,6 +24,49 @@ function createWindow() {
       })
     );
   }
+ {/* TODO: FIX SUBMENU TYPE ERROR*/}
+// const isMac = process.platform === 'darwin'
+const template: Electron.MenuItemConstructorOptions[] = [{
+  label: 'Edit',
+  submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'pasteandmatchstyle' },
+      { role: 'delete' },
+      { role: 'selectall' }
+  ]
+},
+{label: 'View',
+submenu: [
+    { role: 'reload' },
+    { role: 'forcereload' },
+    { role: 'toggledevtools' },
+    { type: 'separator' },
+    { role: 'resetzoom' },
+    { role: 'zoomin' },
+    { role: 'zoomout' },
+    { type: 'separator' },
+    { role: 'togglefullscreen' }
+]
+},
+{ role: 'window', submenu: [{ role: 'minimize' }, { role: 'close' }] },
+{
+role: 'help',
+submenu: [{
+    label: 'Learn More',
+    click() {
+        require('electron').shell.openExternal('https://electron.atom.io');
+    }
+}]
+}
+];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+   
   
   mainWindow.on('closed', () => {
     mainWindow = null;
