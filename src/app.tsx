2dec90645ff93c2c1a30c626b72163f110139393
Nyandika Donald <donald.nyandika@gmail.com>
2020-10-03 15:49:14 +0300

    Use component instead

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
 import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDom from 'react-dom';
-import CodeEditor from './components/CodeEditor'
-import EditorLayout from './components/EditorLayout'
+import CodeEditor from './components/CodeEditor';
+import EditorLayout from './components/EditorLayout';
 
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
 
 const App = () => {
   return (
-   <EditorLayout><CodeEditor/></EditorLayout>
-  )
-}
+    <EditorLayout>
+      <CodeEditor />
+    </EditorLayout>
+  );
+};
 ReactDom.render(<App />, mainElement);
